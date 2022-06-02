3ad8506eaf416504a31737fc2d7f8d49cae6045b
Mokhtar <Moumokhtar@users.noreply.github.com>
2022-06-02 12:41:11 +0200

    Packages
    updated packages and opt in for react-hook-form instead of Formik, updating the components and logic yet to come

--- src/App.js
+++ src/App.js
@@ -1,68 +1,68 @@
-import { useState } from 'react'
-import { AdvancedForm } from './components/forms/AdvancedForm'
+import { useState } from "react";
+// import { AdvancedForm } from './components/forms/AdvancedForm'
 
 export default function App() {
-  const [formValues, setFormValues] = useState([])
+	const [formValues, setFormValues] = useState([]);
 
-  const handleSubmit = async (values, { setSubmitting }) => {
-    setSubmitting(true)
-    setFormValues(values)
-    await new Promise((r) => setTimeout(r, 1000))
-    setSubmitting(false)
-  }
+	// const handleSubmit = async (values, { setSubmitting }) => {
+	//   setSubmitting(true)
+	//   setFormValues(values)
+	//   await new Promise((r) => setTimeout(r, 1000))
+	//   setSubmitting(false)
+	// }
 
-  const formSchema = [
-    { name: 'name', label: 'Name', componentType: 'text', required: true },
-    { name: 'playable', label: 'Playable', componentType: 'checkbox' },
-    {
-      name: 'race',
-      label: 'Race',
-      componentType: 'radioGroup',
-      options: [
-        { label: 'Human', value: 'human' },
-        { label: 'Dwarf', value: 'dwarf' },
-        { label: 'Elf', value: 'elf' },
-      ],
-    },
-    {
-      name: 'class',
-      label: 'Class',
-      componentType: 'select',
-      options: [
-        { label: 'Ranger', value: 'ranger' },
-        { label: 'Wizard', value: 'wizard' },
-        { label: 'Healer', value: 'healer' },
-      ],
-    },
-    {
-      name: 'spell',
-      label: 'Spell',
-      componentType: 'select',
-      options: [
-        { label: 'Fire', value: 'fire' },
-        { label: 'Ice', value: 'ice' },
-      ],
-      condition: { key: 'class', value: 'wizard', operator: '=' },
-    },
-    {
-      name: 'description',
-      label: 'Description',
-      componentType: 'textarea',
-    },
-  ]
+	const formSchema = [
+		{ name: "name", label: "Name", componentType: "text", required: true },
+		{ name: "playable", label: "Playable", componentType: "checkbox" },
+		{
+			name: "race",
+			label: "Race",
+			componentType: "radioGroup",
+			options: [
+				{ label: "Human", value: "human" },
+				{ label: "Dwarf", value: "dwarf" },
+				{ label: "Elf", value: "elf" },
+			],
+		},
+		{
+			name: "class",
+			label: "Class",
+			componentType: "select",
+			options: [
+				{ label: "Ranger", value: "ranger" },
+				{ label: "Wizard", value: "wizard" },
+				{ label: "Healer", value: "healer" },
+			],
+		},
+		{
+			name: "spell",
+			label: "Spell",
+			componentType: "select",
+			options: [
+				{ label: "Fire", value: "fire" },
+				{ label: "Ice", value: "ice" },
+			],
+			condition: { key: "class", value: "wizard", operator: "=" },
+		},
+		{
+			name: "description",
+			label: "Description",
+			componentType: "textarea",
+		},
+	];
 
-  return (
-    <>
-      <h1>Advanced Form</h1>
+	return (
+		<>
+			<h1>Advanced Form</h1>
 
-      <div className="flex">
-        <div className="form section">
-          <AdvancedForm schema={formSchema} onSubmit={handleSubmit} />
-        </div>
-        <div className="results section">
-          <pre>{JSON.stringify(formValues, null, 2)}</pre>
-        </div>
-      </div>
-    </>
-  )
+			<div className="flex">
+				<div className="form section">
+					{/* <AdvancedForm schema={formSchema} onSubmit={handleSubmit} /> */}
+				</div>
+				<div className="results section">
+					<pre>{JSON.stringify(formValues, null, 2)}</pre>
+				</div>
+			</div>
+		</>
+	);
 }
